a4be8032310c318f592c2250c2ce7b88c4240dfe
Tanawy <amreltanawy@hotdesk-app.com>
2023-09-17 15:42:06 +0300

    initial setup of project

---
+++ hotel_room_reservation/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from hotel_room_reservation.users.models import User
+from hotel_room_reservation.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()

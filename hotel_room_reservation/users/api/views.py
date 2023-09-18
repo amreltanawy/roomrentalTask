55e366d4bf14ff368b96c33f5146cae4c3af5b3b
Tanawy <amreltanawy@hotdesk-app.com>
2023-09-18 18:38:16 +0300

    add user registration and allow for user fetching given staff or admin

--- hotel_room_reservation/users/api/views.py
+++ hotel_room_reservation/users/api/views.py
@@ -17,9 +17,22 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
 
     def get_queryset(self, *args, **kwargs):
         assert isinstance(self.request.user.id, int)
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            return self.queryset.all()
         return self.queryset.filter(id=self.request.user.id)
 
     @action(detail=False)
     def me(self, request):
         serializer = UserSerializer(request.user, context={"request": request})
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+    @action(detail=False, methods=["POST"])
+    def register(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user_exists = User.objects.filter(email=serializer.validated_data["email"]).first()
+            if user_exists is not None:
+                return
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
